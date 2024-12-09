0339c3503b6e754c70735c5c93266c663ec65f2a
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-12-09 05:22:55 -0300

    feat: solving problem 3

---
+++ list_1/ex03/main.py
@@ -0,0 +1,8 @@
+N1: float = float(input("Digite o primeiro número: "))
+N2: float = float(input("Digite o segundo número: "))
+N3: float = float(input("Digite o terceiro número: "))
+N4: float = float(input("Digite o quarto número: "))
+
+average = (N1 + N2 + N3 + N4) / 4
+
+print(f"A média aritimética é {average:.2f}")
