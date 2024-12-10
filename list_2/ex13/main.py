790f7edfbf13a554b720f6bc8b3d70908817b1a4
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-12-10 19:54:03 -0300

    feat: solving problem 13

---
+++ list_2/ex13/main.py
@@ -0,0 +1,16 @@
+n: int = int(input("Digite o valor de N: "))
+
+total_even = quantity_even = average = 0
+
+for i in range(n):
+    number: float = float(input(f"Digite o {i + 1}° número: "))
+
+    if number % 2 == 0:
+        total_even += number
+        quantity_even += 1
+
+average = total_even / quantity_even
+
+print(
+    f"Soma total dos números pares: {total_even}\nA média aritimética dos números pares é: {average}"
+)
